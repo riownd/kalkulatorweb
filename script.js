7489fc5693c404c9eef4f2f195e71c7abd118516
user's <macbook@drep.local>
2024-11-10 17:43:51 +0700

    add calculateResult but error

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ function clearDisplay() {
     display.value = '';
 }
 
-function calculateResult() {
-    try {
-        display.value = eval(display.value);
-    } catch (e) {
-        display.value = 'Error';
+function calculateResult(buttons) {
+    if (buttons > 0) {
+      return "The number is positive.";
+    } else if (buttons < 0) {
+      return "The number is negative.";
+    } else {
+      return "The number is zero.";
     }
-}
+  }
